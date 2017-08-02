36035719787830c1dd642ff7e8dc9121df94162e
DamaZeSzczurem <czarna.kawe.prosze@gmail.com>
2017-08-02 07:13:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
 var mainState = {  
     preload: function() {  
         // Here we preload the assets
+        game.load.image('player','assets/sprite.png');
+        game.load.image('wall', 'assets/wall.png');
+        game.load.image('coin', 'assets/points.png');
+    	game.load.image('enemy', 'assets/sprite2.png');    
     },
 
     create: function() {  
-        // Here we create the game
+		// Here we create the game
+ 		//Tworze tło
+        game.stage.backgroundColor = '#3598db';
+        //uruchamiamy fizyke gry
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        //fizyka uruchamiana jest dla wszystkich obiektów
+        game.world.enableBody= true;
+        //Umiejscowienie plejera w grze
+        this.player=game.add.sprite(70,100,'player');
+        //spadanie naszego plejera
+        this.player.body.gravity.y= 600;
+
     },
 
     update: function() {  
