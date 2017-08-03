dcc506d628760d2c3073d677b63a163fb6b81028
DamaZeSzczurem <czarna.kawe.prosze@gmail.com>
2017-08-03 15:19:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,55 @@ var mainState = {
         this.player=game.add.sprite(70,100,'player');
         //spadanie naszego plejera
         this.player.body.gravity.y= 200;
+        //łączenie elementów w grupy
+        this.walls = game.add.group();
+        this.enemies = game.add.group();
+        this.coins = game.add.group();
+        //robimy poziom, tworzymy level 
+        // x-oznacza ściane o-coin !-enemy
+        var level = [
+        'x x x x x x x x x x',
+        '                   ',
+        '                x x',
+        'x x                ',
+        '                   ',
+        '         o         ',
+        '                   ',
+        '                   ',
+        '          x x      ',
+        '          x x      ',
+        'x x x x x x x x x x',
+        ];
+        //sprawdzanie naszej tablicy, jaki jest znak?
+        for (var i = 0; i < level.length; i++) {
+   			 for (var j = 0; j < level[i].length; j++) {
 
-    },
+        // Create a wall and add it to the 'walls' group
+        	if (level[i][j] == 'x') {
+            var wall = game.add.sprite(30+20*j, 30+20*i, 'wall');
+            wall.width = 40;
+            wall.height = 25;
+            this.walls.add(wall);
+            wall.body.immovable = true; 
+        	}
+
+        // Create a coin and add it to the 'coins' group
+        	else if (level[i][j] == 'o') {
+            var coin = game.add.sprite(30+20*j, 30+20*i, 'coin');
+            this.coins.add(coin);
+        	}
+
+        // Create a enemy and add it to the 'enemies' group
+        	else if (level[i][j] == '!') {
+            var enemy = game.add.sprite(30+20*j, 30+20*i, 'enemy');
+            this.enemies.add(enemy);
+        	}
+    }
+}
+},
+        
+
+    
 
     update: function() {  
        if (this.cursor.left.isDown){
@@ -36,14 +83,40 @@ var mainState = {
        else{
        	this.player.body.velocity.x=0;
        }
-        
-        if (this.cursor.up.isDown && this.player.body.touching.down) {
-        	this.player.body.velocity.y= -250;
+
+       if(this.cursor.up.isDown) {
+    		this.player.body.velocity.y = -250;
         }
-    },
+        
+        
+
+
+      // Make the player and the walls collide
+				game.physics.arcade.collide(this.player, this.walls);
+
+// Call the 'takeCoin' function when the player takes a coin
+				game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
+
+// Call the 'restart' function when the player touches the enemy
+				game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
+
+
+// Function to kill a coin
+
+
+// Function to restart the game
+
+ },
+takeCoin: function(player, coin) {
+    coin.kill();
+},
+restart: function() {
+    game.state.start('main');
+},
+
 };
 
 // Initialize the game and start our state
-var game = new Phaser.Game(500, 200);  
+var game = new Phaser.Game(800,300);  
 game.state.add('main', mainState);  
 game.state.start('main');
