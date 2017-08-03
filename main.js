3a0a3ae3f9d3a78c71c6bdbd4baee8d083649605
DamaZeSzczurem <czarna.kawe.prosze@gmail.com>
2017-08-03 07:23:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -16,15 +16,30 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         //fizyka uruchamiana jest dla wszystkich obiektów
         game.world.enableBody= true;
+        this.cursor = game.input.keyboard.createCursorKeys();
+        //zmienna w której będę przechowywać dane o tym 
+        //jaki przycisk jest wciśnięty
         //Umiejscowienie plejera w grze
         this.player=game.add.sprite(70,100,'player');
         //spadanie naszego plejera
-        this.player.body.gravity.y= 600;
+        this.player.body.gravity.y= 200;
 
     },
 
     update: function() {  
-        // Here we update the game 60 times per second
+       if (this.cursor.left.isDown){
+       	  this.player.body.velocity.x= -200;
+       }
+       else if (this.cursor.right.isDown) {
+       	  this.player.body.velocity.x= 200;
+       }
+       else{
+       	this.player.body.velocity.x=0;
+       }
+        
+        if (this.cursor.up.isDown && this.player.body.touching.down) {
+        	this.player.body.velocity.y= -250;
+        }
     },
 };
 
